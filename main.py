1aca4aa40caad68339172fe05371d2c36e90ac49
Ilya Antonov <antonovip2008@gmail.com>
2026-07-11 12:38:43 +0300

    style: refine main window QSS background color style

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from ui_main import MainWindow
 def main():
     app = QApplication(sys.argv)
     
-    # Применение красивого стиля QSS по совету препода
     app.setStyleSheet("""
-        QMainWindow { background-color: #f5f5f5; }
+        QMainWindow { background-color: ##fafafa; }
         QPushButton { 
             font-size: 13px; font-weight: bold; padding: 6px 12px; 
             border-radius: 4px; min-height: 25px; background-color: #0078D4; color: white;
